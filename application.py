4f434d39b6f47998f72d94442657683fc163dde6
Nguyễn Thanh Sơn <son.thanh.97@gmail.com>
2019-07-25 10:29:15 +0700

    Complete Channel list and Channel creation

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import collections
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,6 +9,23 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 
+channels = collections.OrderedDict()
+
 @app.route("/")
 def index():
     return render_template('home.html')
+
+@app.route('/channels', methods=['GET', 'POST'])
+def channels_index():
+    if request.method == 'POST':
+        channelname = request.form.get('channelname')
+
+        # Channel already existed
+        if channelname in channels:
+            flash('A channel with a same name has already existed.')
+
+        # Create a new channel
+        else:
+            channels[channelname] = []
+
+    return render_template('channels.html', channels=channels)
