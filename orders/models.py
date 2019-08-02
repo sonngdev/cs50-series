fb0e5659c7e57d7b074f5293121493c894b5d882
Nguyễn Thanh Sơn <son.thanh.97@gmail.com>
2019-08-02 14:54:23 +0700

    Complete Adding Items

--- orders/models.py
+++ orders/models.py
@@ -7,20 +7,31 @@ class RegularPizza(models.Model):
     large_price = models.FloatField(null=True, blank=True)
     topping_size = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 class SicilianPizza(models.Model):
     name = models.CharField(max_length=64)
     small_price = models.FloatField(null=True, blank=True)
     large_price = models.FloatField(null=True, blank=True)
-    topping_size = models.IntegerField(default=0)
+    item_size = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 class Topping(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 class Sub(models.Model):
     name = models.CharField(max_length=64)
     small_price = models.FloatField(null=True, blank=True)
     large_price = models.FloatField(null=True, blank=True)
-    extra_cheese = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 class SubAddon(models.Model):
     sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name='addons')
@@ -28,15 +39,27 @@ class SubAddon(models.Model):
     small_price = models.FloatField(null=True, blank=True)
     large_price = models.FloatField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Pasta(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Salad(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class DinnerPlatter(models.Model):
     name = models.CharField(max_length=64)
     small_price = models.FloatField(null=True, blank=True)
     large_price = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
